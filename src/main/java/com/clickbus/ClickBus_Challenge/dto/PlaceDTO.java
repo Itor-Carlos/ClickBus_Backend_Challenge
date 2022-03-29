5848b4c356a7e7cc9247115a4910e9a2cfd90ff9
Itor-Carlos <itor51carlos@gmail.com>
2022-03-29 18:27:00 -0300

    Created class PlaceDTO

---
+++ src/main/java/com/clickbus/ClickBus_Challenge/dto/PlaceDTO.java
@@ -0,0 +1,41 @@
+package com.clickbus.ClickBus_Challenge.dto;
+
+public class PlaceDTO {
+
+    private String name;
+    private String city;
+    private String state;
+
+    public PlaceDTO() {
+    }
+
+    public PlaceDTO(String name, String city, String state) {
+        this.name = name;
+        this.city = city;
+        this.state = state;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+}
