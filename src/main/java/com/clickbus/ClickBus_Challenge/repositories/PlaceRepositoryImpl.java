836dad3c8b8760edf77b092fe5a9d9af37c166bd
Itor-Carlos <itor51carlos@gmail.com>
2022-03-30 18:24:36 -0300

    Created class PlaceRepositoryImpl

---
+++ src/main/java/com/clickbus/ClickBus_Challenge/repositories/PlaceRepositoryImpl.java
@@ -0,0 +1,8 @@
+package com.clickbus.ClickBus_Challenge.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository //This annotation indicates that Class is a repository and will be used for more complex queries
+public class PlaceRepositoryImpl {
+}
+
