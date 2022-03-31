277a4f856bf21e6b546e273cace5c706481c6d33
Itor-Carlos <itor51carlos@gmail.com>
2022-03-31 19:23:35 -0300

    interface PlaceRepository extends PagingAndSortingRepository

--- src/main/java/com/clickbus/ClickBus_Challenge/repositories/PlaceRepository.java
+++ src/main/java/com/clickbus/ClickBus_Challenge/repositories/PlaceRepository.java
@@ -2,9 +2,10 @@ package com.clickbus.ClickBus_Challenge.repositories;
 
 import com.clickbus.ClickBus_Challenge.model.Place;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository //This annotation indicates that Interface is a repository and will be used for queries
-//this interface extend JpaRepostiry and will be use in more simples queries
-public interface PlaceRepository extends JpaRepository<Place,Long> {
+//this interface extend JpaRepostiry and will be use in more simples queries and extend the interface PagingAndSortingRepository (will be used to display chunks of data of suitable sizes with high readability
+public interface PlaceRepository extends JpaRepository<Place,Long>, PagingAndSortingRepository<Place,Long> {
 }
