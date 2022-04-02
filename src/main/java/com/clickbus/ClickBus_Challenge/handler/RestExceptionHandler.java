586d9a98ad97be189e346fde744783e441154819
Itor-Carlos <itor51carlos@gmail.com>
2022-04-01 22:10:22 -0300

    add method placeNotFound in class RestExceptionHandler

--- src/main/java/com/clickbus/ClickBus_Challenge/handler/RestExceptionHandler.java
+++ src/main/java/com/clickbus/ClickBus_Challenge/handler/RestExceptionHandler.java
@@ -1,7 +1,26 @@
 package com.clickbus.ClickBus_Challenge.handler;
 
+import com.clickbus.ClickBus_Challenge.exceptions.PlaceNotFoundException;
+import com.clickbus.ClickBus_Challenge.exceptions.PlaceNotFoundExceptionDetails;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.LocalDateTime;
 
 @RestControllerAdvice//Is to catch exceptions thrown by all controllers annotated with @RestController or @ControllerAdvice
 public class RestExceptionHandler {
+
+    //This method is gonna catch a Exceptions of type PlaceNotFoundException and return a ResponseEntity<PlaceNotFoundExceptionDetails> with the informations about the exception
+    @ExceptionHandler(PlaceNotFoundException.class)
+    public ResponseEntity<PlaceNotFoundExceptionDetails> placeNotFoundException(PlaceNotFoundException placeNotFoundException) {
+        PlaceNotFoundExceptionDetails placeNotFoundExceptionDetails = new PlaceNotFoundExceptionDetails(
+                "Place not find in this slug",
+                HttpStatus.NOT_FOUND.value(),
+                LocalDateTime.now(),
+                "PlaceNotFoundException"
+        );
+        return new ResponseEntity<>(placeNotFoundExceptionDetails, HttpStatus.NOT_FOUND);
+    }
 }
