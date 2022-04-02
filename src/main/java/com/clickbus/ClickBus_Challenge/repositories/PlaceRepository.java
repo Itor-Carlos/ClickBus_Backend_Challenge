0b51f30036e2dddb929fe5353f7b532bb8f4f5de
Itor-Carlos <itor51carlos@gmail.com>
2022-04-02 09:46:24 -0300

    add method getBySlug in interface PlaceRepository

--- src/main/java/com/clickbus/ClickBus_Challenge/repositories/PlaceRepository.java
+++ src/main/java/com/clickbus/ClickBus_Challenge/repositories/PlaceRepository.java
@@ -8,4 +8,7 @@ import org.springframework.stereotype.Repository;
 @Repository //This annotation indicates that Interface is a repository and will be used for queries
 //this interface extend JpaRepostiry and will be use in more simples queries and extend the interface PagingAndSortingRepository (will be used to display chunks of data of suitable sizes with high readability
 public interface PlaceRepository extends JpaRepository<Place,Long>, PagingAndSortingRepository<Place,Long> {
+
+    //Method to search a specific Place in database, this method receive a slug and use that to search a place
+    Place getBySlug(String slugRequest);
 }
