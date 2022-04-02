957a723d12d75a8e25a076e47e90923f81195199
Itor-Carlos <itor51carlos@gmail.com>
2022-04-02 09:49:54 -0300

    add method getBySlug in class PlaceService

--- src/main/java/com/clickbus/ClickBus_Challenge/service/PlaceService.java
+++ src/main/java/com/clickbus/ClickBus_Challenge/service/PlaceService.java
@@ -1,10 +1,12 @@
 package com.clickbus.ClickBus_Challenge.service;
 
+import com.clickbus.ClickBus_Challenge.exceptions.PlaceNotFoundException;
 import com.clickbus.ClickBus_Challenge.model.Place;
 import com.clickbus.ClickBus_Challenge.repositories.PlaceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service//This annotation indicates that the Class is a Service and will be used to define the business rules
@@ -18,5 +20,14 @@ public class PlaceService {
         return this.placeRepository.findAll(pageable);//return the Page<Place> for the caller 
     }
 
-
+    //This method is used to search a specific Place. In case the Place is not found, throws a PlaceNotFoundException
+    public Place getBySlug(String slugRequest){
+        Place place = this.placeRepository.getBySlug(slugRequest);
+        if(place != null){
+            return place;
+        }
+        else{
+            throw new PlaceNotFoundException("Place Not Found");
+        }
+    }
 }
