d960bd4b7b1e9fc97afe84a8bb074bd5dd01f932
Itor-Carlos <itor51carlos@gmail.com>
2022-04-06 14:58:52 -0300

    removed message attribute from class Place

--- src/main/java/com/clickbus/ClickBus_Challenge/exceptions/ExceptionDetails.java
+++ src/main/java/com/clickbus/ClickBus_Challenge/exceptions/ExceptionDetails.java
@@ -5,29 +5,18 @@ import java.time.LocalDateTime;
 public class ExceptionDetails {//this class will be used for inheritance, i.e. other classes will extend from this class
 
     /*ATTRIBUTES*/
-    private String message;
     private Integer httpStatus;
     private LocalDateTime timeStamp;
     private String exception;
 
     /*CONSTRUCTOR*/
-    public ExceptionDetails(String message, Integer httpStatus, LocalDateTime timeStamp, String exception) {
-        this.message = message;
+    public ExceptionDetails(Integer httpStatus, LocalDateTime timeStamp, String exception) {
         this.httpStatus = httpStatus;
         this.timeStamp = timeStamp;
         this.exception = exception;
     }
 
     /*GETTERS E SETTERS*/
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
     public Integer getHttpStatus() {
         return httpStatus;
     }
