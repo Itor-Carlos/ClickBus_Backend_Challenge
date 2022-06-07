3b99b62e1628972a6c5ea2030412bcc336d74ceb
Itor Carlos Souza Queiroz <72228871+Itor-Carlos@users.noreply.github.com>
2022-06-07 18:52:50 -0300

    added method searchPlaces in class PlaceRepositoryImpl

--- src/main/java/com/clickbus/ClickBus_Challenge/repositories/PlaceRepositoryImpl.java
+++ src/main/java/com/clickbus/ClickBus_Challenge/repositories/PlaceRepositoryImpl.java
@@ -1,9 +1,16 @@
 package com.clickbus.ClickBus_Challenge.repositories;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import org.springframework.stereotype.Repository;
+
+import com.clickbus.ClickBus_Challenge.model.Place;
 
 @Repository //This annotation indicates that Class is a repository and will be used for more complex queries
 public class PlaceRepositoryImpl {
@@ -11,5 +18,43 @@ public class PlaceRepositoryImpl {
 
     @PersistenceContext
     private EntityManager entityManager;
+
+    public List<Place> searchPlaces(Long id, String name, String state, String city, String slug){
+        StringBuilder jpql = new StringBuilder();
+        jpql.append("from Place WHERE 0 = 0");
+
+        Map<String, Object> mapParameter = new HashMap<>();
+
+        if(id != null){
+            jpql.append(" and id = :id");
+            mapParameter.put("id", id);
+        }
+        
+        if(name != null){
+            jpql.append(" and name LIKE :name");
+            mapParameter.put("name", name.toLowerCase()+"%");
+        }
+
+        if(state != null){
+            jpql.append(" and state LIKE :state");
+            mapParameter.put("state", state.toLowerCase()+"%");
+        }
+
+        if(city != null){
+            jpql.append(" and city LIKE :city");
+            mapParameter.put("city", city.toLowerCase()+"%");
+        }
+
+        if(slug != null){
+            jpql.append(" and slug = :slug");
+            mapParameter.put("slug", slug);
+        }
+
+        TypedQuery<Place> query = this.entityManager.createQuery(jpql.toString(), Place.class);
+        mapParameter.forEach((key,value) -> query.setParameter(key, value));
+
+        return query.getResultList();
+    } 
+
 }
 
